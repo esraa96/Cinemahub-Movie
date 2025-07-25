ebedc37e79cf5d9ad8fe350a5408d7be6b1ba3c5
Esraa Awad <9esrawad@gmail.com>
2025-07-25 19:46:05 +0300

    your commit message

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,8 @@ const nextConfig = {
     ],
   },
   experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs']
+  serverExternalPackages: ['@prisma/client'],
+
   }
 };
 
